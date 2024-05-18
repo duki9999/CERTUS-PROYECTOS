c95c3b224ccfcfc316be342dffad8b26ba2667dd
BySweetie <61038091@certus.edu.pe>
2024-05-18 09:34:16 -0500

    miau

---
+++ CALCULADORA 2022/script.js
@@ -0,0 +1,14 @@
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function calculate() {
+    var display = document.getElementById('display').value;
+    var result = eval(display);
+    document.getElementById('display').value = result;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
